d6fc8cc97522487e85f68b98a0cecf803674a9c9
kato <kato@e-catchup.jp>
2017-08-08 19:51:53 +0900

    fix ryuring-17730  /lib/Baser/Config/Theme/　多言語化

--- lib/Baser/Config/theme/bc_sample/Elements/smartphone/widgets/blog_monthly_archives.php
+++ lib/Baser/Config/theme/bc_sample/Elements/smartphone/widgets/blog_monthly_archives.php
@@ -48,9 +48,9 @@ $baseCurrentUrl = $this->params['Content']['name'] . '/archives/date/';
 					<?php $class = '' ?>
 				<?php endif ?>
 				<?php if ($view_count): ?>
-					<?php $title = $postedDate['year'] . '年' . $postedDate['month'] . '月' . '(' . $postedDate['count'] . ')' ?>
+					<?php $title = $postedDate['year'] . __('年') . $postedDate['month'] . __('月') . '(' . $postedDate['count'] . ')' ?>
 				<?php else: ?>
-					<?php $title = $postedDate['year'] . '年' . $postedDate['month'] . '月' ?>
+					<?php $title = $postedDate['year'] . __('年') . $postedDate['month'] . __('月') ?>
 				<?php endif ?>
 				<li<?php echo $class ?>>
 					<?php $this->BcBaser->link($title, $this->request->params['Content']['url'] . '/archives/date/' . $postedDate['year'] . '/' . $postedDate['month']) ?>
